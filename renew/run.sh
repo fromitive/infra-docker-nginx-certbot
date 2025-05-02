bfd9b662db948503889646cfd87765c97029930c
MooSong Lee <fromitive@gmail.com>
2025-05-02 10:30:11 +0900

    fix: certbot 재발급 할 수 없었던 오류 수정

--- renew/run.sh
+++ renew/run.sh
@@ -6,7 +6,7 @@ ABSOLUTE_SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 cd "${ABSOLUTE_SCRIPT_DIR}" || exit 1
 
-set -e 
+set -e
 
 if [ -z "$1" ]; then
   echo "Usage: ./run.sh [applied_ssl_nginx_container_name]"
@@ -22,8 +22,10 @@ docker compose run certbot
 echo "[+] apply new certificate"
 docker exec $CONTAINER_NAME nginx -s reload
 
+echo "[-] stop nginx for cert"
+docker compose down
+
 echo "change all certification permission"
 find "$CERTIFICATIONS_PATH" -type f -exec chmod 400 {} \;
-
 
 cd "${ORIGINAL_DIR}"
